5c092cddc3b11d00455bc246cae4944164addc3d
peter-keller <keller.f.peter@gmail.com>
2019-04-20 15:05:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // Write your solution here!
+
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
+
+/*
 const pets = ["Milo", "Otis", "Garfield"];
+
 
 const append = [...pets, "Odie"];
 let prepend = ["Odie", ...pets];
 let removeLast = pets.slice(0, -1);
 let removeFirst = pets.slice(1, pets.length);
+*/
