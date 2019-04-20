a0f10bd62eaade36753b4f2554ea675fa6595726
peter-keller <keller.f.peter@gmail.com>
2019-04-20 15:01:25 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+const pets = ["Milo", "Otis", "Garfield"];
+
+const append = [...pets, "Odie"];
+let prepend = ["Odie", ...pets];
+let removeLast = pets.slice(0, -1);
+let removeFirst = pets.slice(1, pets.length);
